f396184665daeb2d09e95784ffb45f12c8c9bd17
krtEngineer <krt.engineer@gmail.com>
2024-02-08 11:46:19 +0530

    Adding authorization header.
    Signed-off-by: krtEngineer <krt.engineer@gmail.com>

--- src/CustomFetch.jsx
+++ src/CustomFetch.jsx
@@ -1,9 +1,12 @@
 import axios from "axios";
 
 const customFetch = axios.create({
-  baseURL: `https://api.unsplash.com/search/photos?client_id=${
-    import.meta.env.VITE_UNSPLASH_CLIENT_ACCESS_KEY
-  }&query=`,
+  baseURL: `https://api.unsplash.com/search/photos?query=`,
+  headers: {
+    Authorization: `Client-ID ${
+      import.meta.env.VITE_UNSPLASH_CLIENT_ACCESS_KEY
+    }`,
+  },
 });
 
 export default customFetch;
